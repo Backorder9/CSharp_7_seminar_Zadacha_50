2f525b8e69a0e56b4022c2c3ec6ffa6defdb87dc
Dmitry Osipovich <dosipovich@hotmail.com>
2022-10-24 11:56:48 +0300

    Задача №50 к 7 семинару решена успешно!

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 50.
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+// int [,] Num = {{1,4,7,2},{5,9,2,3},{8,4,2,4}};
+string ArrayElement(string a, string b)
+{
+   int [,] Num = {{1,4,7,2},{5,9,2,3},{8,4,2,4}};
+   int x = Num.GetLength(0);
+   int y = Num.GetLength(1);
+   int i = int.Parse(a);
+   int j = int.Parse(b);
+   string c = String.Empty;
+   if (i > x-1 || j > y-1) c = "такого числа в массиве нет";
+   else c = Num[i,j].ToString();
+   return c;
+}
+
+Console.WriteLine();
+Console.Write("Введите индекс строки элемента массива: ");
+string m = Console.ReadLine();
+Console.WriteLine();
+Console.Write("Введите индекс столбца элемента массива: ");
+string n = Console.ReadLine();
+Console.WriteLine();
+string L = ArrayElement(m, n);
+Console.Write($"Элемент массива с индексами ({m},{n}): {L}");
